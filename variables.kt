c35f8f86075639ce2ffae569bccaeff100fbc8d6
FRANK MUHINDO <100847088+MfrankUg@users.noreply.github.com>
2025-07-07 15:53:07 +0300

    This is session 1 of Kotlin 100 days

---
+++ variables.kt
@@ -0,0 +1,13 @@
+
+fun main(){
+    // This is the main function
+    println("hello_world")
+    // This is a string defined
+    val name: String = "Jerry"
+    println(name)
+    var a = 1
+    a=2
+    println(a)
+
+
+}
