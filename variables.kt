ea3dfb04821fea4d1d29e55aa622c6aa95253c2e
FRANK MUHINDO <100847088+MfrankUg@users.noreply.github.com>
2025-07-07 17:19:42 +0300

    practice commit changes

--- variables.kt
+++ variables.kt
@@ -1,16 +1,22 @@
 
-fun main(){
+fun main() {
     // This is the main function
     println("hello_world")
     // This is a string defined
     val name: String = "Jerry"
     println(name)
     var a = 1
-    a=2
+    a = 2
     println(a)
-     var b = 2
-       b = a + b
+    var b = 2
+    b = a + b
     // This is for summations
     println(b)
-
+    Cards(test = "test")
 }
+
+
+    fun Cards(test: String){
+
+        println("Hello_2 $test")
+    }
